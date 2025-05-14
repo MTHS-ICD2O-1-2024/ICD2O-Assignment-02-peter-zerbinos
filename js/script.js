112d8c30850626981add2d066ffe76a484a8e28f
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-14 00:42:16 +0000

    added in code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates the volume of a pyramid.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateVolumeOfPyramid () {
+  // input
+  const baseLength = parseFloat(document.getElementById('base-length').value)
+  const baseWidth = parseFloat(document.getElementById('base-width').value)
+  const pyramidHeight = parseFloat(document.getElementById('pyramid-height').value)
+
+  // process
+  const baseArea = baseLength * baseWidth
+  const volumeOfPyramid = (1/3) * baseArea * pyramidHeight
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volumeOfPyramid.toFixed(2) + ' cm³'
 }
